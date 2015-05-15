b08cbefdb18427f293114382d950052e00d2f895
Ji Tao <jt6562@163.com>
2015-05-15 11:58:08 +0800

    support url base64

--- app.py
+++ app.py
@@ -3,21 +3,32 @@
 
 import falcon
 import logging
+import base64
 
-# Falcon follows the REST architectural style, meaning (among
-# other things) that you think in terms of resources and state
-# transitions, which map to HTTP verbs.
+debug = False
+_L = str
+if debug:
+    file_h = logging.FileHandler("log")
+    logger = logging.getLogger('wrapweb.'+__name__)
+    logger.addHandler(file_h)
+    logger.setLevel(logging.DEBUG)
+    _L = logger.debug
+
+
 class WebResource:
-    def __init__(self):
-        self.logger = logging.getLogger('wrapweb.'+__name__)
-
     def on_get(self, req, resp):
         """Handles GET requests"""
-        url = req.params.get('url', '')
-        self.logger.error(req.params)
-        self.logger.info(url)
+        _L(req.params)
+        b64url = req.params.get('url', '')
+        _L(b64url)
+        try:
+            url = base64.b64decode(b64url)
+        except:
+            url = b64url
+        _L("url:"+url)
+        _L("UA"+req.user_agent)
 
-        body_style = "overflow:hidden; margin:0px; padding:0px; "
+        body_style = 'overflow:hidden; margin:0px; padding:0px;'
         iframe_style = 'width:100%; height:100%; border:0px; margin:0px; padding:0px;'
 
         resp.status = falcon.HTTP_200  # This is the default status
@@ -27,16 +38,14 @@ class WebResource:
                      '  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'
                      '</head>'
                      '<body style="%s">'
-                     '  <iframe src="%s" style="%s">'
+                     '  <iframe src="%s" style="%s" seamless>'
                      '  </iframe>'
                      '</body>'
                      '</html>' % (body_style, url, iframe_style))
 
-# falcon.API instances are callable WSGI apps
+
 app = falcon.API()
 
-# Resources are represented by long-lived class instances
 web = WebResource()
 
-# things will handle all requests to the '/wrapweb' URL path
 app.add_route('/wrapweb', web)
